83de9949c9ecea3a568a0b167f57319845dae1b1
ronicairo <roni.cairo@outlook.fr>
2025-12-09 19:19:33 +0100

    Initial Commit

---
+++ MyGymTrackerTests/MyGymTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyGymTrackerTests.swift
+//  MyGymTrackerTests
+//
+//  Created by Roni Cairo on 09/12/2025.
+//
+
+import Testing
+
+struct MyGymTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
